8045c782aa35715af22b72698282e8c0e82aa41b
Dan Dobrick & Kyle Dorsey <pair+dandobrick.kylepdorsey@devbootcamp.com>
2016-09-27 17:55:35 -0500

    Completed comment board 1

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -3,9 +3,24 @@ get '/posts' do
   erb :'/posts/index'
 end
 
+get '/posts/new' do
+  erb :'/posts/new'
+end
+
 get '/posts/:id' do
   @post = Post.find(params[:id])
   @comments = @post.comments
   erb :'/posts/show'
 end
+
+post '/posts' do
+  post = Post.new(body: params[:body], user_id: current_user.id, title: params[:title])
+  if post.save
+    redirect "/posts/#{post.id}"
+  else
+    @errors = "Something went wrong!"
+    erb :'/posts/new'
+  end
+end
+
 
